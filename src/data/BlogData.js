aaf4688aafb7acd147473c03208e0c4ec9d1a60d
yaswanthteja <yaswanthteja02@gmail.com>
2022-05-23 15:49:00 +0530

    first commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,71 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"How to Install Windows 11 Lite on Your PC ???",
+        tags:["windows","windows 11 Lite","os"],
+        date:"15 Jan, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--zoUikeVI--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/phghcjdbu0jnk6akki4y.jpg",
+        link:"https://dev.to/yaswanthteja/how-to-install-windows-11-lite-on-your-pc--38o6"
+    },
+    {
+        id:2,
+        name:"Message Encryption-Decryption using Python",
+        tags:["Python","Encryption","Decryption"],
+        date:"15 Jan, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--VPBjHE3F--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/v9w52q2imkfhhbba4r0p.png",
+        link:"https://dev.to/yaswanthteja/message-encryption-decryption-using-python-j5l"
+    },
+    {
+        id:3,
+        name:"Colour Detection using Pandas & OpenCV",
+        tags:["python","pandas","opencv"],
+        date:"24 Feb, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--FIe3718L--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2alkcm9ys32338cbw2ai.png",
+        link:"https://dev.to/yaswanthteja/colour-detection-using-pandas-opencv-17i7"
+    },
+    {
+        id:4,
+        name:"How to Learn Data Science For Free??",
+        tags:["DataScience","Data Analysis","Data Visualization"],
+        date:"10 Apr, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rlFfXgCI--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/o4h80t7rl3n7eyyoz28g.jpg",
+        link:"https://dev.to/yaswanthteja/how-to-learn-data-science-for-free-1c03"
+    },
+    {
+        id:5,
+        name:"Typing Speed Test Game using javascript?",
+        tags:["Html","css","Javascript"],
+        date:"3 Apr, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--tIxayQyz--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/uji3p0z6b75nygmjt7v6.png",
+        link:"https://dev.to/yaswanthteja/typing-speed-test-game-using-javascript-in3"
+    },
+    {
+        id:6,
+        name:"Text To Speech Converter in HTML CSS & JavaScript",
+        tags:["Html","css,","Javascript"],
+        date:"2 Apr, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--LxGjl-k4--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/wrd5uednhy3ci39nc767.png",
+        link:"https://dev.to/yaswanthteja/text-to-speech-converter-in-html-css-javascript-3eme"
+
+    },
+   
+    {
+        id:7,
+        name:"Differences Between Python Modules, Packages, Libraries, and Frameworks",
+        tags:["python","Modules","Packages","Libraries","Frameworks"],
+        date:"5 March, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--2j68lCi2--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/62drfsz62wujoi6szdyl.jpg",
+        link:"https://dev.to/yaswanthteja/differences-between-python-modules-packages-libraries-and-frameworks-37f8"
+    },
+    {
+        id:8,
+        name:"windows Powershell customization without using Windows Subsystem for Linux(wsl)",
+        tags:["powershell","linux","customization"],
+        date:"26 Feb, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--IoJ9l2x---/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/669ohe1h91pc78dykveb.png",
+        link:"https://dev.to/yaswanthteja/windows-powershell-customization-without-wsl-5f26"
+    },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Color Detection using Pandas & OpenCV",
+        description:"Colour detection is necessary to recognize objects, it is also used as a tool in various image editing and drawing apps..",
+        tags:["Colour","Pandas","OpenCV"],
+        
+        demo:"https://res.cloudinary.com/practicaldev/image/fetch/s--FIe3718L--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2alkcm9ys32338cbw2ai.png",
+        github:"https://github.com/yaswanthteja/color_detection-using-pandas-opencv"
+    },
+    {
+        id:2,
+        name:"Typing-speed-tester",
+        description:"Typing speed test is a game where you can check your typing speed like WPM (Word Per Minute), CPM (Character Per Minute), Accuracy, etc.",
+        tags:["Html","Css","javascript","Typing"],
+        
+        demo:"https://vercel.com/yaswanthteja/typing-speed-test",
+        github:"https://github.com/yaswanthteja/Typing-speed-test"
+        
+    },
+    {
+        id:3,
+        name:"Text-To-Speech-Converter",
+        description:"Text To Speech (TTS) is a technology that enables your text to be converted into speech sounds. In this project (Text To Speech Converter App).",
+        tags:["TTS","api","speech","javascript"],
+        
+        demo:"https://vercel.com/yaswanthteja/text-to-speech-converter/",
+        github:"https://github.com/yaswanthteja/Text-To-Speech-Converter"
+    },
+    {
+        id:4,
+        name:"Paint",
+        description:"Paint is a replica of MS Paint, is a simple program that allows users to create basic graphic art on a computer.",
+        tags:["Paint","mspaint","replica","javascript"],
+        
+        demo:"https://vercel.com/yaswanthteja/paint",
+        github:"https://github.com/yaswanthteja/Paint"
+       
+    },{
+        id:5,
+        name:"Message Encryption-Decryption",
+        description:"Message encoding and decoding is the process to first convert the original text to the random and meaningless text called ciphertext. .",
+        tags:["python","encrption","decryption","ciphertext"],
+        
+        demo:"https://res.cloudinary.com/practicaldev/image/fetch/s--VPBjHE3F--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/v9w52q2imkfhhbba4r0p.png",
+        github:"https://github.com/yaswanthteja/-MessageEncrypter-Decrypter"
+        
+    },
+    {
+        id:6,
+        name:"HotelBillingSystem",
+        description:"Hotel billing system is a 'GUI based 'application It uses python and some Libraries like Tkinter and some looping concepts and functions and custom fonts.",
+        tags:["python","tkinter","looping","custom fonts"],
+        
+        demo:"https://youtu.be/QTD381WeZ8c",
+        github:"https://github.com/yaswanthteja/HotelBillingSystem"
+        
+    },
+    
+  
+] 
