a29dfcb6968aa19b77e7e6d27f9bd516784c035e
Yaswanth Teja Yarlagadda <93423367+yaswanthteja@users.noreply.github.com>
2025-08-11 12:12:55 +0530

    Update SocialIcons.js

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -46,7 +46,7 @@ const SocialIcons = (props) => {
             animate={{scale:[0,1,1.5,1]}}
             transition={{type:'spring', duration:1, delay:1.2}}
             >
-                <NavLink style={{color:'inherit'}}  target="_blank"   to={{pathname:"https://twitter.com/yaswanthteja02"}}>
+                <NavLink style={{color:'inherit'}}  target="_blank"   to={{pathname:"https://x.com/y_ai_sh"}}>
                     <Twitter width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text  : DarkTheme.body  } />
                 </NavLink>
             </motion.div>
